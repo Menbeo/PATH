e49ffe66285460804ee18fd1bd44ff3102601509
leoleo <qp713223@gmail.com>
2025-07-08 02:24:38 +0700

    modify

--- Dijkstra.py
+++ Dijkstra.py
@@ -2,26 +2,13 @@ from gridmaplv3 import create_grid_map,grid_map, default_goal, default_start
 import numpy as np 
 import matplotlib.pyplot as plt
 
-def animate_path(grid,path,delay=0.01):
-    plt.figure(figsize=(10,10))
-    plt.imshow(grid, cmap='gray_r', origin='upper')
-    plt.plot(default_start[1], default_goal[0], 'go', marketsize=10, label='Start')
-    plt.plot(default_goal[1],default_start[0],'ro', marketsize=10, label='Goal')
-    plt.legend()
-    for i in range(1,len(path)):
-        x0,y0 = path[i-1]
-        x1,y1 = path[i]
-        plt.plot([y0, y1], [x0,x1], 'b-', linewidth=2)
-        plt.pause(delay)
-    plt.grid(True)
-    plt.show()
 def dijkstra(start, goal, grid):
     rows, cols = grid.shape
     visited = set()
     distance = {start: 0}
     previous = {}
     queue = [start]
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] 
     while queue:
         current = min(queue, key=lambda node: distance.get(node, float('inf')))
         queue.remove(current)
@@ -51,8 +38,28 @@ def dijkstra(start, goal, grid):
     path.reverse()
     return path
 
+def animate_path(grid,path,delay=1):
+    plt.figure(figsize=(10,10))
+    plt.imshow(grid, cmap='gray_r', origin='upper')
+    plt.plot(default_start[1], default_start[0], 'go', markersize=10, label='Start')
+    plt.plot(default_goal[1],default_goal[0],'ro', markersize=10, label='Goal')
+    plt.legend()
+    for i in range(1,len(path)):
+        x0,y0 = path[i-1]
+        x1,y1 = path[i]
+        plt.plot([y0, y1], [x0,x1], 'b-', linewidth=2)
+        plt.pause(delay)
+    plt.grid(True)
+    plt.show()
+
 if __name__ == "__main__":
     grid = grid_map()
     path = dijkstra(default_start, default_goal, grid)
-    animate_path(grid,path)
-    create_grid_map(grid, path)
+    if not path:
+        print("No path")
+    else:
+        animate_path(grid,path)
+        create_grid_map(grid, path)
+        
+
+
