281ad469348c45f6a2bc2bdb830788da629cea02
leoleo <qp713223@gmail.com>
2025-07-14 10:41:51 +0700

    verify dijkstra done

--- Dijkstra.py
+++ Dijkstra.py
@@ -10,11 +10,48 @@ def Dijkstra(grid, start, goal):
     #declare rows and cols 
     rows,cols = grid.shape
     visited = set()
+    previous = {}
     distance = {start:0}
     pq = [(0, start)]
+    directions = [(-1, 0), (1,0), (0,-1), (0,1)]
 
     while pq:
         cost, current = heapq.heappop(pq)
+        if current in visited:
+            continue
+        visited.add(current)
+
+        if current == goal:
+            break
+
+        for dx, dy in directions:
+            neighbor = current[0] + dx, current[1] + dy
+            if 0 <= neighbor[0] < rows and 0 <= neighbor[1] < cols:
+                if grid[neighbor] == 0: 
+                    new_cost= cost + 1
+                    if new_cost < distance.get(neighbor, float('inf')):
+                        distance[neighbor] = new_cost
+                        previous[neighbor] = current
+                        heapq.heappush(pq, (new_cost, neighbor))
+        
+    path = []
+    node = goal 
+    while node != start:
+            path.append(node)
+            node = previous.get(node)
+            if node is None:
+                return []
+    path.append(start)
+    path.reverse()
+    return path
         
 
-        
+if __name__ == "__main__":
+    for map_id in range(1,5):
+        grid = grid_map(map_id=map_id)
+        path = Dijkstra(grid,default_start, default_goal)
+        if not path: 
+            print("no path")
+        else:
+            create_grid_map(grid, path)
+
