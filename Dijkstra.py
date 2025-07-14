bd69028ab24a3544f4d491d1da79fcc016c8f998
leoleo <qp713223@gmail.com>
2025-07-14 16:00:05 +0700

    update gridmap + dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,5 +1,6 @@
 #import gridmap
-from gridmap import create_grid_map, grid_map, default_goal,default_start,convert_grid_to_lat_lon
+from gridmap import create_grid_map, grid_map, default_goal,default_start
+from gridmap import convert_grid_to_lat_lon,compute_neighborhood_layers
 import numpy as np 
 import matplotlib.pyplot as plt
 import heapq
@@ -15,6 +16,9 @@ def Dijkstra(grid, start, goal):
     pq = [(0, start)]
     directions = [(-1, 0), (1,0), (0,-1), (0,1)]
 
+    #Get the inflation layers: 
+    inflation = compute_neighborhood_layers(grid,max_layer=3)
+
     while pq:
         cost, current = heapq.heappop(pq)
         if current in visited:
@@ -27,8 +31,17 @@ def Dijkstra(grid, start, goal):
         for dx, dy in directions:
             neighbor = current[0] + dx, current[1] + dy
             if 0 <= neighbor[0] < rows and 0 <= neighbor[1] < cols:
-                if grid[neighbor] == 0: 
-                    new_cost= cost + 1
+                if grid[neighbor] == 0:    
+                    #Assign cost based on danger zone 
+                    layer = inflation[neighbor]
+                    if layer == 1:
+                        layer_cost = 100
+                    elif layer == 2:
+                        layer_cost = 2
+                    else:
+                        layer_cost = 1 
+
+                    new_cost= cost + layer_cost
                     if new_cost < distance.get(neighbor, float('inf')):
                         distance[neighbor] = new_cost
                         previous[neighbor] = current
@@ -37,10 +50,10 @@ def Dijkstra(grid, start, goal):
     path = []
     node = goal 
     while node != start:
-            path.append(node)
-            node = previous.get(node)
-            if node is None:
-                return []
+        path.append(node)
+        node = previous.get(node)
+        if node is None:
+            return []
     path.append(start)
     path.reverse()
     return path
@@ -54,4 +67,7 @@ if __name__ == "__main__":
             print("no path")
         else:
             create_grid_map(grid, path)
+            lat_lon_path = [convert_grid_to_lat_lon(x,y) for x,y in path]
+            filename = f"Dijkstra_map{map_id}.waypoints"
+            export_waypoints(lat_lon_path, filename=filename)
 
--- convert_to_waypoints.py
+++ convert_to_waypoints.py
@@ -1,7 +1,16 @@
+import numpy as np
 def export_waypoints(lat_lon_path: list[tuple[float,float]], filename  = "Dijkstra.waypoints", default_altitude=1000):
+    num_original_waypoints = len(lat_lon_path)
+    num_export_waypoints = min(num_original_waypoints, 12)
+    if num_export_waypoints > 1:
+        indices_to_export = np.linspace(0, num_original_waypoints - 1, num_export_waypoints, dtype=int)
+    else: # If only one waypoint, take the first one
+        indices_to_export = [0] if num_original_waypoints > 0 else []
+    # Filter the path to include only the selected waypoints
+    sampled_lat_lon_path = [lat_lon_path[i] for i in indices_to_export]
     with open(filename, 'w') as f:
         f.write("QGC WPL 110 \n")
-        for i, (lat,lon) in enumerate(lat_lon_path):
+        for i, (lat,lon) in enumerate(sampled_lat_lon_path):
             waypoint_index = i
             is_current = 1 if i == 0 else 0
             autocontinue = 3
