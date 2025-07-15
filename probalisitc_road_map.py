e5c3f8cd5eaa64a0e18aa2b7bacef114f5e45c90
leoleo <qp713223@gmail.com>
2025-07-16 02:12:44 +0700

    update PRM

--- probalisitc_road_map.py
+++ probalisitc_road_map.py
@@ -5,11 +5,15 @@ import numpy as np
 import random 
 import math 
 import networkx as nx 
-from Dijkstra import Dijkstra
+
+inflation = compute_neighborhood_layers(grid, max_layer=3)
+
 def is_free(x, y, grid):
     x = int(x)
     y = int(y)
-    return 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1] and grid[x, y] == 0
+    if 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1] and grid[x, y] == 0:
+        return False
+    
 
 def line_free(p1, p2, grid):
     steps = int(max(abs(p1[0]-p2[0]), abs(p1[1]-p2[1]))) + 1
@@ -31,49 +35,50 @@ def sample_points(n, grid):
     return samples
 
 def connect_nodes(samples, radius, grid):
-    G = nx.Graph()
+    graph = {i: [] for i in range(len(samples))}
     for i, p1 in enumerate(samples):
-        G.add_node(i, pos=p1)
         for j, p2 in enumerate(samples):
-            if i != j and math.dist(p1,p2) <= radius:
-                if line_free(p1,p2,grid):
-                    G.add_edge(i,j, weight=math.dist(p1,p2))
-    return G
+            if i != j and math.dist(p1, p2) <= radius:
+                if line_free(p1, p2, grid):
+                    weight = math.dist(p1, p2)
+                    graph[i].append((j, weight))  # ✅ tuple
+    return graph
 
-# def dijkstra(graph, start_idx,  goal_idx):
-#     dist = {i: float('inf') for i in graph}
-#     prev = {}
-#     dist[start_idx] = 0
-#     visited = set()
+def dijkstra(graph, start_idx,  goal_idx):
+    dist = {i: float('inf') for i in graph}
+    prev = {}
+    dist[start_idx] = 0
+    visited = set()
 
-#     while True:
-#         current = None
-#         min_dist = float('inf')
-#         for node in graph:
-#             if node not in visited and dist[node] < min_dist:
-#                 current = node
-#                 min_dist = dist[node]
-#         if current is None or current == goal_idx:
-#             break
-#         visited.add(current)
-#         for neighbor, weight in graph[current]:
-#             if neighbor in visited:
-#                 continue
-#             new_dist = dist[current] + weight
-#             if new_dist < dist[neighbor]:
-#                 dist[neighbor] = new_dist
-#                 prev[neighbor] = current
+    while True:
+        current = None
+        min_dist = float('inf')
+        for node in graph:
+            if node not in visited and dist[node] < min_dist:
+                current = node
+                min_dist = dist[node]
+        if current is None or current == goal_idx:
+            break
+        visited.add(current)
 
-#     # Reconstruct path
-#     path = []
-#     node = goal_idx
-#     while node in prev:
-#         path.append(node)
-#         node = prev[node]
-#     if path:
-#         path.append(start_idx)
-#         path.reverse()
-#     return path
+        for neighbor, weight in graph[current]:
+            if neighbor in visited:
+                continue
+            new_dist = dist[current] + weight 
+            if new_dist < dist[neighbor]:
+                dist[neighbor] = new_dist
+                prev[neighbor] = current
+
+    # Reconstruct path
+    path = []
+    node = goal_idx
+    while node in prev:
+        path.append(node)
+        node = prev[node]
+    if path:
+        path.append(start_idx)
+        path.reverse()
+    return path
 
 if __name__ == "__main__":
     for map_id in range(1, 5):
@@ -86,9 +91,9 @@ if __name__ == "__main__":
         start_idx = len(samples) - 2
         goal_idx = len(samples) - 1
 
-        G = connect_nodes(samples, radius=50, grid=grid)
+        graph = connect_nodes(samples, radius=50, grid=grid)
         try:
-            path_idx = nx.dijkstra_path(G, source=start_idx, target=goal_idx)
+            path_idx = dijkstra(graph, start_idx, goal_idx)
             path = [samples[i] for i in path_idx]
             lat_lon_path = [
                     convert_grid_to_lat_lon(point_y, point_x) for point_x, point_y in path
