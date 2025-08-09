ce600b6a1ec2d79613faaa21edc1cd07462386d7
leoleo <qp713223@gmail.com>
2025-08-10 04:32:34 +0700

    update check

--- new_apply.py
+++ new_apply.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 def angle_between(v1, v2):
     v1 = v1 / np.linalg.norm(v1)
     v2 = v2 / np.linalg.norm(v2)
@@ -16,3 +17,18 @@ def turn_constraint(path, angle_threshold=10):
             smoothed.append(path[i])
     smoothed.append(path[-1])
     return smoothed
+
+def bspline_smooth(path, smoothing_factor=0, num_points=100):
+    path = np.array(path)
+    x = path[:, 0]
+    y = path[:, 1]
+    
+    # Parameterize the path
+    t = np.linspace(0, 1, len(path))
+    
+    # B-spline representation
+    tck, _ = interpolate.splprep([x, y], s=smoothing_factor)
+    u_fine = np.linspace(0, 1, num_points)
+    x_fine, y_fine = interpolate.splev(u_fine, tck)
+    
+    return np.column_stack((x_fine, y_fine))
